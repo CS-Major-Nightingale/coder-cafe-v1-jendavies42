13536a7168c7be1714dd45c862d9374b63786ce4
jenniferdavies <jdavies@nightingale.org>
2025-09-30 17:55:43 -0400

    1

--- src/CoderCafe.java
+++ src/CoderCafe.java
@@ -2,7 +2,28 @@
 
 public class CoderCafe {
     public static void main(String args[]) {
-        // jen is great
-        // Good Luck!
+        // border
+        System.out.println("☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️");
+        // write out jens cafe and welcome message
+        System.out.println("Welcome to");
+        System.out.print("Jen's Coder Cafe!");
+        // month-date-year hour:min:sec
+
+        //set vars: myMatcha, myLatte, myCap, myCoco
+        //print vars "x item" then spaced out price
+            // get spacing and stars right
+
+        //set var for sub
+            //"Subtotal: "subtotal -> addition of all prices
+                // spaces and stars still
+        // set var for sales
+            // "Sales tax:" 8.875% of all items to the cent(num*100)/100.00
+        // set var for service fee
+            // "Service fee:" 1.21 added to complete cost
+
+        // total: addition of sub,sales,service fees
+
+        // goodbye message
+        // star line
     }
 }
