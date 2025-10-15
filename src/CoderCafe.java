5664891b1eb4ed0d92680b69dd99db26ef533450
jenniferdavies <jdavies@nightingale.org>
2025-10-15 11:52:58 -0400

    hi finished corrections

--- src/CoderCafe.java
+++ src/CoderCafe.java
@@ -3,100 +3,109 @@
 import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
 import java.time.LocalDateTime; // Import the LocalDateTime class
 
-public class CoderCafe {
-    public static void main(String args[]) {
 
-        // border
+public class CoderCafe{
+
+    public static void main(String[] args) {
+
+        //Establish amount of each item
+        int myMatcha = 2;
+        int myLatte = 4;
+        int myCap = 3;
+        int myCoco = 1;
+
+        //Establish price for each item
+        double priceMatcha = 4.50;
+        double priceLatte = 5.30;
+        double priceCap = 3.75;
+        double priceCoco = 4.80;
+
+        // Establish total price of item
+        double totalMatcha = myMatcha * priceMatcha;
+        double totalLatte = myLatte * priceLatte;
+        double totalCap = myCap * priceCap;
+        double totalCoco = myCoco * priceCoco;
+
+        //Establish subtotal
+        double subTotal = totalMatcha + totalLatte + totalCap + totalCoco;
+
+        //Establish sales tax
+        double salesT = subTotal * .08875;
+        double salesTax = (int) (salesT * 100) / 100.0;
+
+        //Establish service fee
+        double serviceFee = 1.21;
+
+        //Establish cafe tax
+        double quantityItems = myMatcha + myLatte + myCap + myCoco;
+        double unroundedCafeFee = subTotal % quantityItems;
+        double cafeFee = (int) (unroundedCafeFee * 100) / 100.0;
+
+        //Establish total price
+        double totalTot = subTotal + salesTax + serviceFee + cafeFee;
+
+        // Print top border
         System.out.println(" ");
         System.out.println(" ☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️☕️☕️☕️☕️☕️☕️☕️☕️️");
 
-        // write out jens cafe and welcome message
+        // Print welcome message
         System.out.println("-\t\t\tWelcome to Jen's Coder Cafe!\t\t\t-");
 
-        // month-date-year hour:min:sec
-
+        // Print time and date
         LocalDateTime myDateObj = LocalDateTime.now();
         DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("-\t\t\t      dd-MM-yyyy HH:mm:ss\t\t\t\t-");
 
         String formattedDate = myDateObj.format(myFormatObj);
         System.out.println(formattedDate);
 
-        //set vars: myMatcha, myLatte, myCap, myCoco (represent amount of each item)
-        int myMatcha = 2;
-        int myLatte = 4;
-        int myCap = 3;
-        int myCoco = 1;
-
-        //set price for each item by first letter
-        double priceM = 4.50;
-        double priceL = 5.30;
-        double priceCa = 3.75;
-        double priceHc = 4.80;
-
-        //space line
+        //Empty line
         System.out.println("-\t\t\t\t\t\t\t\t\t\t\t\t\t-");
 
-        //print vars "x item" then spaced out price
-        System.out.printf("- \t\t" + myMatcha + " x Matchas " + "\t\t\t\t  $ %.2f " + "\t\t- ", myMatcha * priceM);
+        //Print amount of item and cost
+        System.out.printf("- \t\t" + myMatcha + " x Matchas " + "\t\t\t\t  $ %.2f " + "\t\t- ", totalMatcha );
 
         System.out.println(" ");
-        System.out.printf("- \t\t" + myLatte + " x Lattes" + "\t\t\t\t\t  $ %.2f" + "\t\t- ", myLatte * priceL);
+        System.out.printf("- \t\t" + myLatte + " x Lattes" + "\t\t\t\t\t  $ %.2f" + "\t\t- ", totalLatte);
 
         System.out.println(" ");
-        System.out.printf("- \t\t" + myCap + " x Cappuccinos" + "\t\t\t\t  $ %.2f " + "\t\t- " , myCap * priceCa);
+        System.out.printf("- \t\t" + myCap + " x Cappuccinos" + "\t\t\t\t  $ %.2f " + "\t\t- " , totalCap);
 
         System.out.println(" ");
-        System.out.printf("- \t\t" + myCoco + " x Hot Chocolates" + "\t\t\t  $ %.2f " + "\t\t- ", myCoco * priceHc);
+        System.out.printf("- \t\t" + myCoco + " x Hot Chocolates" + "\t\t\t  $ %.2f " + "\t\t- ", totalCoco);
 
-        //set var for sub totals
-        double totalM = myMatcha * priceM;
-        double totalL = myLatte * priceL;
-        double totalCa = myCap * priceCa;
-        double totalHc = myCoco * priceHc;
-
-        double subTotal = totalM + totalL + totalCa + totalHc;
-
-        //Subtotal -> addition of all prices
+        //Print filler line
         System.out.println(" ");
         System.out.println("-\t\t\t\t\t\t\t\t\t\t\t\t\t-");
 
+        //Print subtotal
         System.out.println("- \t\t" + "Subtotal: \t\t\t\t\t  $ " + subTotal + "\t\t-");
 
-        // set var for sales tax
-        double salesTax = subTotal * .08875;
-        double salesT = (int) (salesTax * 100) / 100.0;
+        //Print sales tax
+        System.out.printf("- \t\t" + "Sales tax:" + "\t\t\t\t\t  $ %.2f " + "\t\t-", salesTax);
 
-        // "Sales tax:" 8.875% of all items to the cent
-        System.out.printf("- \t\t" + "Sales tax:" + "\t\t\t\t\t  $ %.2f " + "\t\t-", salesT);
-
-        // set var for service fee
-        double serviceFee = 1.21;
-
-        // "Service fee:" 1.21 added to complete cost
+        //Print service fee
         System.out.println(" ");
         System.out.println("- \t\t" + "Service fee: \t\t\t\t  $ " + serviceFee + "\t\t-");
 
-       // cafe tax for wi-fi -> divide sub by num
-        double myNum = myMatcha + myLatte + myCap + myCoco;
-        double cafeTax = (int) ((subTotal / myNum)*100)/100.0;
-        System.out.printf("- \t\t" + "Cafe fee:" + "\t\t\t\t\t  $ %.2f" + "\t\t-", cafeTax );
+        //Print cafe fee
+        System.out.printf("- \t\tCafe fee:\t\t\t\t\t  $ %.2f \t\t-", cafeFee);
 
-        // print total: addition of sub,sales,service, cafe fees
-        double totalTot = subTotal + salesT + serviceFee + cafeTax;
-        System.out.println(" ");
-        System.out.println("-\t\t\t\t\t\t\t\t\t\t\t\t\t- ");
-        System.out.printf("- \t\t\t\t" + "Total Price: $%.2f" + "\t\t\t\t    -", totalTot);
-
-        // space between sections
+        //Print filler lines
         System.out.println(" ");
         System.out.println("-\t\t\t\t\t\t\t\t\t\t\t\t\t- ");
 
-        // goodbye message
-        System.out.println("- \t\t\t   " + "Thank you come again!" + "\t\t\t\t-");
+        //Print total price
+        System.out.printf("- \t\t\t\tTotal Price: $%.2f\t\t\t\t    -", totalTot);
 
+        //Print filler lines
+        System.out.println(" ");
         System.out.println("-\t\t\t\t\t\t\t\t\t\t\t\t\t- ");
 
-        // outro receipt line
+        //Print goodbye message
+        System.out.println("- \t\t\t   Thank you come again!\t\t\t\t-");
+        System.out.println("-\t\t\t\t\t\t\t\t\t\t\t\t\t- ");
+
+        //Print bottom border
         System.out.println(" ☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️☕️️☕️☕️☕️☕️☕️☕️☕️☕️");
     }
     }
